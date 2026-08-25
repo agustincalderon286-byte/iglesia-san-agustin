82f670458d437415ecae9e57b016f1528d495f21
agustincalderon286-byte <agustincalderon286@gmail.com>
2026-08-24 20:19:49 -0500

    Actualizar caché de la aplicación

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'san-agustin-v3';
+const CACHE = 'san-agustin-v4';
 const OFFLINE = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => event.waitUntil(
